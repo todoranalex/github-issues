4efef52575be66111157eb8a6f8fde5ef09af58e
Alex Todoran <alextodoran@192.168.1.5>
2022-07-10 14:58:46 +0300

    chore: setup jest coverage

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,31 @@
+function getCoverageReporters() {
+  let coverageReporters = ['html'];
+
+  if (process.env.COV_CONSOLE_OUTPUT === 'true') {
+    // Show coverage in console output, but hide report lines for fully-covered files
+    // Useful when you want to collect coverage for a file in isolation
+    coverageReporters = [...coverageReporters, ['text', {skipFull: true}]];
+  }
+
+  return coverageReporters;
+}
+
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFiles: ['./__tests__/setup.js'],
+  // collectCoverageFrom: ['<rootDir>/__tests__'],
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: getCoverageReporters(),
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   testPathIgnorePatterns: [
     '\\.snap$',
     '<rootDir>/node_modules/',
